596d72588e63948a7191e691598c2dcb8a4a6bd1
André Roque Matheus <amatheus@mac.com>
2017-04-10 15:19:53 -0300

    Jenkins support

--- back/src/main/java/br/fatecsp/engsoft/DetetiveApplication.java
+++ back/src/main/java/br/fatecsp/engsoft/DetetiveApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DetetiveApplication {
-	private DetetiveApplication() {}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DetetiveApplication.class, args);
 	}
