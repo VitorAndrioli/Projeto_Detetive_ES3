17cf4d1590eadf520c017473c794f8206fed5410
André Roque Matheus <amatheus@mac.com>
2017-04-10 22:38:31 -0300

    Jenkins support

---
+++ back/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -o errexit
+
+MVN_VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+mv target/detetive-${MVN_VERSION}.jar /opt/detetive
