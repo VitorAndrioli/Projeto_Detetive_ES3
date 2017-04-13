434c9fb839f0c151f6b3c60e13a04200ecf414e0
teious <talesgp_96@hotmail.com.br>
2017-04-13 14:30:53 -0300

    tela login

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {MaterialModule}  from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { LoginComponent} from  './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import 'hammerjs';
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
+    LoginComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
