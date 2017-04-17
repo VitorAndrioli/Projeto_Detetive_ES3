7c0aeac9c38f0ac4db4db9242dbf9e9c0c606028
teious <talesgp_96@hotmail.com.br>
2017-04-17 10:48:34 -0300

    code review

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {User } from '../models/user';
-import {AuthService} from '../auth.service'
+import { User } from '../models/user';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit  {
+export class LoginComponent implements OnInit {
 
-    public user = new User('','', false);
+    public user = new User('', '', false);
     public errorMsg = '';
- 
-    constructor( private auth:AuthService) {}
 
- ngOnInit(){
-   this.auth.checkLoginCredentials();
- }
+    constructor(private auth: AuthService) { }
+
+    ngOnInit() {
+        this.auth.checkLoginCredentials();
+    }
     login() {
-        if(!this.auth.login(this.user)){
+        if (!this.auth.login(this.user)) {
             this.errorMsg = 'email ou senha inválidos';
         }
     }
